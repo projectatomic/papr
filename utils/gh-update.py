56e9ec5c2750fb742c4328a8ce43f2898d24fa69
Stephen Milner <stevem@gnulinux.net>
2016-10-08 19:06:47 -0400

    gh-update.py: pep8 fixes and Python 2/3 support
    * gh-update.py: support Python 2 and 3
    
    * gh-update.py: pep8 fixes

--- utils/gh-update.py
+++ utils/gh-update.py
@@ -1,14 +1,14 @@
-#!/bin/python
+#!/usr/bin/env python
+"""
+Small utility to facilitate updating GitHub commit CI
+status: https://developer.github.com/v3/repos/statuses/
 
-'''
-    Small utility to facilitate updating GitHub commit CI
-    status: https://developer.github.com/v3/repos/statuses/
+Parameters are passed on the command-line. They can be
+prefixed by 'env:' to denote a lookup in an environment
+variable. E.g. --token env:token.
+"""
 
-    Parameters are passed on the command-line. They can be
-    prefixed by 'env:' to denote a lookup in an environment
-    variable. E.g. --token env:token.
-'''
-
+from __future__ import print_function
 
 import os
 import sys
@@ -37,10 +37,9 @@ def main():
 
 
 def parse_args():
-
     """
-        Parses program arguments and optionally resolves
-        pointers to environment variables.
+    Parses program arguments and optionally resolves
+    pointers to environment variables.
     """
 
     parser = argparse.ArgumentParser()
@@ -59,24 +58,24 @@ def parse_args():
             if val.startswith('env:'):
                 new_val = os.environ.get(val[4:])
                 if new_val is None and arg in required_args:
-                    print "Parameter '%s' is required, but the given" % arg,
-                    print "environment variable '%s' is missing." % val[4:]
-                    return None
+                    parser.error(
+                        "Parameter '%s' is required, but the given "
+                        "environment variable '%s' is missing." % (
+                            arg, val[4:]))
                 setattr(args, arg, new_val)
             # we allow users to pass "" for optional vars to mean None so that
             # they don't have to resort to e.g. eval
             elif val == "":
                 if arg in required_args:
-                    print "Parameter '%s' is required, but the given" % arg,
-                    print "argument is empty."
-                    return None
+                    parser.error(
+                        "Parameter '%s' is required, but the given "
+                        "argument is empty." % arg)
                 setattr(args, arg, None)
 
     return args
 
 
 def craft_data_dict(state, context, description, url):
-
     "Creates the data dictionary as required by the API."
 
     data = {'state': state}
@@ -90,25 +89,25 @@ def craft_data_dict(state, context, description, url):
 
 
 def update_status(repo, commit, token, data):
-
     "Sends the status update's data using the GitHub API."
 
     token_header = {'Authorization': 'token ' + token}
     api_url = ("https://api.github.com/repos/%s/statuses/%s" %
                (repo, commit))
 
-    print "Updating status of commit", commit, "with data", data
+    print("Updating status of commit", commit, "with data", data)
 
     # use data= instead of json= in case we're running on an older requests
     resp = requests.post(api_url, data=json.dumps(data), headers=token_header)
 
-    if resp.status_code != requests.codes.created: # pylint: disable=no-member
-        print "Failed to update commit status [HTTP %d]" % resp.status_code
-        print resp.headers
-        print resp.json()
+    if resp.status_code != requests.codes.created:  # pylint: disable=no-member
+        print("Failed to update commit status [HTTP %d]" % resp.status_code)
+        print(resp.headers)
+        print(resp.json())
         return False
 
     return True
+
 
 if __name__ == '__main__':
     sys.exit(main())
