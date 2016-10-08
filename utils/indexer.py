e21c60fb03e56865f8a2884d274eb9840d1baf3d
Steve Milner <smilner@redhat.com>
2016-10-08 09:42:51 -0400

    indexer.py: Typo and formatting fixes.

--- utils/indexer.py
+++ utils/indexer.py
@@ -1,23 +1,46 @@
 #!/usr/bin/env python
-
-'''
-    Recursively create index.html file listings for
-    directories that do no have any.
-'''
+# MIT License
+#
+# Copyright (c) 2016 Jonathan Lebon
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+"""
+Recursively create index.html file listings for
+directories that do not have any.
+"""
 
 from os import getcwd, listdir
 from os.path import isfile, isdir, join
 from cgi import escape
 
+
 def get_index(dirpath):
+    "Attempts to find an index file"
     if isfile(join(dirpath, 'index.htm')):
         return 'index.htm'
     if isfile(join(dirpath, 'index.html')):
         return 'index.html'
     return None
 
-def create_index(dirpath, at_top):
 
+def create_index(dirpath, at_top):
+    "Creates a new index.html file"
     # get children
     names = []
     for name in listdir(dirpath):
@@ -56,6 +79,7 @@ def create_index(dirpath, at_top):
 
         f.write("</body></html>")
 
+
 def recurse(dirpath):
     for name in listdir(dirpath):
         path = join(dirpath, name)
@@ -64,7 +88,14 @@ def recurse(dirpath):
                 create_index(path, at_top=False)
                 recurse(path)
 
-cwd = getcwd()
-if get_index(cwd) is None:
-    create_index(cwd, at_top=True)
-    recurse(cwd)
+
+def main():
+    "Main entry point"
+    cwd = getcwd()
+    if get_index(cwd) is None:
+        create_index(cwd, at_top=True)
+        recurse(cwd)
+
+
+if __name__ == '__main__':
+    main()
