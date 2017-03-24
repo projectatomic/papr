projectatomic/papr

commit c7cf12cf6a737f4c969119f6e50033f7e1f4408f

parser.py: don't shell-escape env values
We were overly cautious here with quoting shell values. Really, this
can only affect the target environment in which the test runs, so
there's no possible way to use this pre-"chroot". This allows us to
define things that refer to other variables, e.g.:

env:
  PATH: $HOME/gopath/bin:$PATH

utils/parser.py
@@ -187,7 +187,7 @@ def flush_suite(suite, outdir):
     if 'env' in suite:
         envs = ''
         for k, v in suite['env'].items():
-            envs += 'export %s=%s\n' % (k, shlex.quote(v))
+            envs += 'export %s=%s\n' % (k, v)
         write_to_file(outdir, "envs", envs)
 
     if 'build' in suite:

commit 694a6d6138bd021b4e50bad373c92686d3fdc6da

env: wrap value around quotes
Otherwise, values that have e.g. spaces won't parse out. This is a
regression from c7cf12c. Now, users can specify vars that reference
other vars

env:
  MYVAR: $HOME/subdir

and vars that have spaces:

env:
  MYVAR: asdf zxcv

Users can enforce literal strings by wrapping in single quotes, e.g.:

env:
  MYVAR: "'a literal dollar sign $'"

utils/parser.py
@@ -187,7 +187,7 @@ def flush_suite(suite, outdir):
     if 'env' in suite:
         envs = ''
         for k, v in suite['env'].items():
-            envs += 'export %s=%s\n' % (k, v)
+            envs += 'export %s="%s"\n' % (k, v)
         write_to_file(outdir, "envs", envs)
 
     if 'build' in suite: